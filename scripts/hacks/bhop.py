ee757422b3307b05c00e58f13c8287b47baa08e0
saiko-2043 <bessapachecolr@gmail.com>
2021-06-12 19:40:06 +0100

    general push

--- scripts/hacks/bhop.py
+++ scripts/hacks/bhop.py
@@ -4,10 +4,11 @@ import time
 def enableBhop(dwForceJump, dwLocalPlayer, m_fFlags, pm, client):
 
     if keyboard.is_pressed("space"):
-        force_jump = client + dwForceJump
-        player = pm.read_int(client + dwLocalPlayer)
-        grounded = pm.read_int(player + m_fFlags)
-        if player and grounded and grounded == 257:
-            pm.write_int(force_jump, 5)
-            time.sleep(0.08)
-            pm.write_int(force_jump, 4)
+            force_jump = client + dwForceJump
+            player = pm.read_int(client + dwLocalPlayer)
+            if player:
+                on_ground = pm.read_int(player + m_fFlags)
+                if on_ground and on_ground == 257:
+                    pm.write_int(force_jump, 5)
+                    time.sleep(0.08)
+                    pm.write_int(force_jump, 4)
