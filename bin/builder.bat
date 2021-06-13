a72340d46985c77068ef575ecb76ca5ef0174750
saiko-2043 <bessapachecolr@gmail.com>
2021-06-13 17:31:31 +0100

    updated builder

--- bin/builder.bat
+++ bin/builder.bat
@@ -1,3 +1,19 @@
 @echo off
 
+TITLE pyHax builder
+COLOR 2
+
+goto INIT
+
+REM USE --noconsole to build without the console. Not ideal for debugging
+
+:INIT
+
+set /p val = "Do you have pyinstaller installed? <y/n> "
+
+if (%val% == "y") (goto BUILDER) else (pip install pyinstaller)
+
+goto BUILDER
+
+:BUILDER
 pyinstaller ..\main.py --onefile --icon ..\assets\icon.ico
