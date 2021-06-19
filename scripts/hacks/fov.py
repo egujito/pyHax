28d2a4c03cb76cd74b8f56d072276ca403bdaecc
saiko-2043 <bessapachecolr@gmail.com>
2021-06-19 19:47:39 +0100

    check if player exists. Else we are either on the lobby or dead

--- scripts/hacks/fov.py
+++ scripts/hacks/fov.py
@@ -1,7 +1,8 @@
 def changeFov(dwLocalPlayer, dwEntityList, m_iFOV, FOV_VAL, pm, client):
     player = pm.read_int(client + dwLocalPlayer)
-    fov = player + m_iFOV
-    pm.write_int(fov, FOV_VAL)
+    if player: # IF WE CAN FIND THE PLAYER. ELSE WE ARE IN THE LOBBY OR DEAD
+        fov = player + m_iFOV
+        pm.write_int(fov, FOV_VAL)
 
     #player = pm.read_int(client + dwEntityList)
     #pm.write_int(player + m_iFOV, FOV_VAL)
